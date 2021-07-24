3d52a85f46d935b922c54b861731d7aa75234305
daniel.paredes <daniel.paredes@q-loud.de>
2021-07-24 13:40:32 +0200

    clean-up: use gitsupport class functions

--- FromNothing/FromNothing.py
+++ FromNothing/FromNothing.py
@@ -1,10 +1,10 @@
 import yaml
 import sys
 import os
-import subprocess
-PIPE = subprocess.PIPE
+import shutil
 
 from .cbuilder import CBuilder
+from .gitsupport import GitSupport
 
 class FromNothing(object):
     def __init__(self, yml_file: str): 
@@ -19,17 +19,6 @@ class FromNothing(object):
         for prj, prj_info in self.projects.items():
             self.__create(prj_info)
 
-    def __create(self, prj):
-        self.current_prj = prj
-        prj_type = prj['type']
-
-        if prj_type == 'c':
-            self.__create_c()
-        elif prj_type == 'bash':
-            self.__create_bash()
-        else:
-            print("Unknow project type")
-
     def __go_to_project_dir(self):
         prj_path = self.current_prj['path'] + '/' + self.current_prj['name']
         try: 
@@ -39,26 +28,27 @@ class FromNothing(object):
         os.chdir(prj_path)
         print(os.getcwd())
 
-    def __create_c(self):
+    def __create(self, prj):
+        self.current_prj = prj
+        prj_type = prj['type']
+
+        gitsupport = GitSupport(self.current_prj['git'],
+                                self.current_prj['name'],
+                                self.current_prj['clone_type'])
+
         self.__go_to_project_dir()
 
-        process = subprocess.Popen(['git', 'init'], stdout=PIPE, stderr=PIPE)
-        stdoutput, stderroutput = process.communicate()
 
-        cbuilder = CBuilder(self.current_prj) 
-        cbuilder.create()
-
-        #TODO: maybe use logging to print everything
-#     # To Create A New GitLab Project
-#     git add --all
-#     git commit -m "New project: ${PRJ_NAME}"
-#     if [ "${SSH}" == "ssh" ]; then
-#         git remote add origin https://"${SERVER_GIT}":"${NAMESPACE}"/"${PRJ_NAME}".git
-#         git push --set-upstream https://"${SERVER_GIT}":"${NAMESPACE}"/"${PRJ_NAME}".git master
-#     else
-#         git remote add origin git@"${SERVER_GIT}":"${NAMESPACE}"/"${PRJ_NAME}".git
-#         git push --set-upstream git@"${SERVER_GIT}":"${NAMESPACE}"/"${PRJ_NAME}".git master
-#     fi
-
-
+        if prj_type == 'c':
+            cbuilder = CBuilder(self.current_prj) 
+            cbuilder.create()
+#         elif prj_type == 'bash':
+#             self.__create_bash()
+        else:
+            print("Unknow project type")
+        
+        gitsupport.init_repo()
+        gitsupport.create_info_files()
+        gitsupport.create_gitlab_cicd_file()
+        gitsupport.push_project()
 
