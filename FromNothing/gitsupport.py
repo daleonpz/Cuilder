153ecb85a1260d05d36a1ad19950909d0f4b4b8a
daniel.paredes <daniel.paredes@q-loud.de>
2021-07-24 17:11:06 +0200

    clean-up: remove git unrelated code

--- FromNothing/gitsupport.py
+++ FromNothing/gitsupport.py
@@ -1,4 +1,3 @@
-import shutil
 import subprocess
 import requests
 PIPE = subprocess.PIPE
@@ -6,58 +5,58 @@ PIPE = subprocess.PIPE
 class GitSupport(object):
 
     def __init__(self, git_info: dict, prjname: str, clone_type: str):
-        self.server = git_info['server'] 
-        self.user = git_info['user']
-        self.api_token = git_info['api_token']
-        self.service = git_info['service']
+        self.git = git_info
         self.prjname = prjname
         self.clone_type = clone_type
         self.authors = None
         if 'authors' in git_info:
             self.authors = git_info['authors']
 
-
     def init_repo(self):
         process = subprocess.Popen(['git', 'init'], stdout=PIPE, stderr=PIPE)
         stdoutput, stderroutput = process.communicate()
-
-    def create_gitlab_cicd_file(self):
-        #TODO: use also template, maybe this in prjbuilder.py
-        print("Creating gitlab CI/CD file...")
-#         shutil.copy( self.current_prj['templates_path']+ '/gitlab-ci.yml',  '.gitlab-ci.yml')
 
     def __create_authors_file(self):
         with open('AUTHORS','w') as f: 
             f.write("AUTHORS:\n")
 
             if self.authors is not None:
-                for x in range(0, len(self.authors['name'])):
-                    f.write("* " + self.authors['name'][x] + " ")
-                    f.write("("  + self.authors['email'][x] + ")\n")
+                for i in range(0, len(self.authors['name'])):
+                    f.write("* " + self.authors['name'][i] + " ")
+                    f.write("("  + self.authors['email'][i] + ")\n")
             else:
                 f.write("<Your-Name> <youre_mail> (your_website)")
 
     def create_info_files(self):
         print("Creating general git files...")
-        open('.gitignore', 'a').close() 
-        open('README', 'a').close() 
-        open('LICENSE', 'a').close() 
-        open('CHANGELOG', 'a').close() 
+        open('.gitignore', 'a').close()
+        open('README', 'a').close()
+        open('LICENSE', 'a').close()
+        open('CHANGELOG', 'a').close()
         self.__create_authors_file()
 
     def push_project(self):
 
-        if self.service == 'github':
+        server = self.git['server'] 
+        user = self.git['user']
+        api_token = self.git['api_token']
+        service = self.git['service']
+
+        if service == 'github':
             r = requests.post('https://api.github.com/user/repos', 
-                    auth=(self.user, self.api_token),
+                    auth=(user, api_token),
                     json = {"name": self.prjname} )
             print("Request Status :")
             print(r.status_code)
 
         process = subprocess.Popen(['git', 'add','--all'], stdout=PIPE, stderr=PIPE)
+        stdoutput, stderroutput = process.communicate()
+        print(stdoutput)
         process = subprocess.Popen(['git', 'commit','--m', 'New Project'], stdout=PIPE, stderr=PIPE)
+        stdoutput, stderroutput = process.communicate()
+        print(stdoutput)
 
-        gitpath = self.server + ":" + self.user + "/" + self.prjname  + '.git'
+        gitpath = server + ":" + user + "/" + self.prjname  + '.git'
         
         if self.clone_type == 'ssh':
             gitpath = 'git@' +  gitpath
@@ -66,5 +65,9 @@ class GitSupport(object):
 
         print('GitPath: ' + gitpath)
         process = subprocess.Popen(['git', 'remote', 'add','origin', gitpath], stdout=PIPE, stderr=PIPE)
+        stdoutput, stderroutput = process.communicate()
+        print(stdoutput)
         process = subprocess.Popen(['git', 'push','--set-upstream', 'origin', 'master'], stdout=PIPE, stderr=PIPE)
+        stdoutput, stderroutput = process.communicate()
+        print(stdoutput)
 
